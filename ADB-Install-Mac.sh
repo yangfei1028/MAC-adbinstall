9e5208212245845411b192dc4333d61fc92f7083
yangfei1028 <yangfei1028@gmail.com>
2015-05-13 16:15:40 +0800

    Update ADB-Install-Mac.sh

--- ADB-Install-Mac.sh
+++ ADB-Install-Mac.sh
@@ -6,10 +6,7 @@
 #  This may be run from anywhere and is compatible with Linux
 #  and Mac OS X. Hopefully a Windows batch file will be available
 #  very soon.
-#
-#  Created by kalaker on 04/20/2012
-#  Updated by kalaker on 05/30/2012
-#  http://forum.xda-developers.com/member.php?u=4510103 <-My XDA Profile
+
 echo "This will install ADB and Fastboot on your computer."
 echo "Root Permissions required. Please type your password."
 sudo cd ~ #Gives Superuser permissions
